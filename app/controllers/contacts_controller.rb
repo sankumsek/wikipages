c1002243825f923659c79797ddf07176b50cdade
Sanjeev Sekar <student@epicodus.com>
2014-08-21 15:04:43 -0700

    Add edit function

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,4 +3,43 @@ class ContactsController < ApplicationController
     @contacts = Contact.all
     render('contacts/index.html.erb')
   end
+
+  def show
+    @contact = Contact.find(params[:id])
+    render('contacts/show.html.erb')
+  end
+
+  def new
+    render('contacts/new.html.erb')
+  end
+
+  def create
+    @contact = Contact.new(:name => params[:name],
+                           :email => params[:email],
+                           :phone => params[:phone])
+    if @contact.save
+      render('contacts/success.html.erb')
+    else
+      render('contacts/new.html.erb')
+    end
+  end
+
+  def create
+    @contact = Contact.create(:name => params[:name],
+                              :email => params[:email],
+                              :phone => params[:phone])
+    render('contacts/success.html.erb')
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    if @contact.update(:name => params[:name],
+                       :email => params[:email],
+                       :phone => params[:phone])
+      render('contacts/success.html.erb')
+    else
+      render('contacts/edit.html.erb')
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   match('contacts', {:via => :get, :to => 'contacts#index'})
+  match('contacts', {:via => :post, :to => 'contacts#create'})
+  match('contacts/new', {:via => :get, :to => 'contacts#new'})
+  match('contacts/:id', {:via => :get, :to => 'contacts#show'})
+  match('contacts/:id/edit', {:via => :get, :to => 'contacts#edit'})
 end
