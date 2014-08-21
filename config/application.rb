b0ff4877428483770713dd333564f1d276608cac
Sanjeev Sekar <student@epicodus.com>
2014-08-21 10:18:14 -0700

    Modify database file

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,6 @@ module Wikipages
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.action_controller.set_new_parameters = true
+    config.action_controller.permit_all_parameters = true
   end
 end
